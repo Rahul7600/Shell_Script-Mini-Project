584ca4d09ece95573886c6db2044367734e9982b
Rahul Dev Ray <85834486+Rahul7600@users.noreply.github.com>
2023-03-08 11:28:45 +0530

    Create password_generator.sh

---
+++ password_generator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Simple Password Generate
+
+echo "Welcome to simple password generate"
+
+sleep 1
+
+echo "Please enter the length of the password"
+read PASS_LENGTH
+
+for p in $(seq 3);
+do
+     openssl rand -base64 48 |cut -c1-$PASS_LENGTH
+done
+
+
+~                                                                                                                                                     
+~                                      
