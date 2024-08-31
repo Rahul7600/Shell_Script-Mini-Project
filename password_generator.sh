d143884601ac54ef3c12c24c6161ee93c7e092dd
Rahul Dev Ray <85834486+Rahul7600@users.noreply.github.com>
2024-08-31 12:20:46 +0530

    Update password_generator.sh

--- password_generator.sh
+++ password_generator.sh
@@ -14,6 +14,7 @@ do
      openssl rand -base64 48 |cut -c1-$PASS_LENGTH
 done
 
+Hello
 
 ~                                                                                                                                                     
 ~                                      
