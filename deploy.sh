06fd2341102ab7ca98ff721bc702c71ea7f2588a
Ubuntu <ubuntu@ip-172-31-8-127.ap-south-1.compute.internal>
2024-08-31 10:21:57 +0000

    deploy.sh pushed

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define your repository URL and path
+REPO_URL="https://github.com/Rahul7600/Shell_Script-Mini-Project.git"
+REPO_PATH="/home/ubuntu/Shell_Script-Mini-Project"
+
+# Navigate to the repository directory
+cd "$REPO_PATH" || exit
+
+# Perform git pull
+git pull origin main
+
+# Optionally, you can add additional commands here, such as restarting services or cleaning up
+
+echo "Deployment completed successfully."
